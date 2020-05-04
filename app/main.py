ee86431640a89243b36b3dde84634a182f872461
Daniel Carmona <Dev.Phantom.UNAM@gmail.com>
2020-05-04 15:59:55 -0500

    Creamos la tabla
    Asignamos encabezados, asignamos informacion, definimos un callback cuando un elemento sea seleccionado. Imprimimos los elementos que seleccionamos.

--- app/main.py
+++ app/main.py
@@ -8,6 +8,9 @@ class PokeDex(toga.App):
         self.title = title
         self.size = (WIDTH, HEIGHT)
 
+        self.heading = ['Name']
+        self.data = ['Python','Ruby','Laravle']
+
         self.create_elements()
     
     def startup(self):
@@ -25,7 +28,12 @@ class PokeDex(toga.App):
         self.create_table()
 
     def create_table(self):
-        self.table = toga.Table(['Encabezado 1','Encabezado 2'])
+        self.table = toga.Table(self.heading, data=self.data, on_select=self.select_element)
+
+    #CALLBACKS
+    def select_element(self,widget,row):
+        if row:
+            print(row.name)
 
 if __name__ == '__main__':
     pokedex = PokeDex('Pokedex','com.devphantom.org')

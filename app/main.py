2cc9bba27503391a31eef8589ebd40d22276964d
Daniel Carmona <Dev.Phantom.UNAM@gmail.com>
2020-05-04 20:49:35 -0500

    Toolbar
    Agregamos una barra para manipular el sigueinte y el previo

--- app/main.py
+++ app/main.py
@@ -26,15 +26,27 @@ class PokeDex(toga.App):
         split.content = [self.table, box]
 
         self.main_window.content = split
+        self.main_window.toolbar.add(self.previus_command, self.next_command)
 
         self.main_window.show()
 
     def create_elements(self):
         self.create_table()
+        self.create_toolbar()
+
+    def create_toolbar(self):
+        self.create_next_command()
+        self.create_previus_command()
+
+    def create_next_command(self):
+        self.next_command = toga.Command(self.next, label='Next', icon=BULBASAUR_ICON)
+
+    def create_previus_command(self):
+        self.previus_command = toga.Command(self.previus, label='Previus', icon=METAPOD_ICON)
+
 
     def create_table(self):
         self.table = toga.Table(self.heading, data=self.data, on_select=self.select_element)
-
 
     def load_async_data(self): 
         thread = threading.Thread(target=self.load_data)
@@ -60,6 +72,12 @@ class PokeDex(toga.App):
     def select_element(self,widget,row):
         if row:
             print(row.name)
+
+    def next(self, widget):
+        print("next")
+    
+    def previus(self,widget):
+        print("Previus")
 
 if __name__ == '__main__':
     pokedex = PokeDex('Pokedex','com.devphantom.org')
